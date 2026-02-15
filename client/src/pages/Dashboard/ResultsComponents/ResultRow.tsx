d0183a33e8cad0d804677ebd64e797419bcfedc0
julsimmy <jesimko@gmail.com>
2026-02-15 15:23:39 -0600

    - Fixed npm issue

--- client/src/pages/Dashboard/ResultsComponents/ResultRow.tsx
+++ client/src/pages/Dashboard/ResultsComponents/ResultRow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { highlightText, toDate, getTypeColor } from "./resultsUtils";
 import type { ResultRow as ResultRowType, EditValues } from "./resultsTypes";
 import { ResultEditForm } from "./ResultEditForm";
